b1a689cd4fe69baca5ecb4bf7c13a25c6080bb98
Manh <manhhubt@yahoo.com>
2019-11-20 10:21:19 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+// Get saved data from sessionStorage
+var nameTheme = sessionStorage.getItem("myTheme");
+if (nameTheme) {
+	$("body")
+		.removeClass()
+		.addClass(nameTheme);
+}
+
+var searchBox = document.querySelectorAll(
+	'.search-box input[type="text"] + span',
+);
+
+searchBox.forEach(elm => {
+	elm.addEventListener("click", () => {
+		console.log(elm.previousElementSibling.value);
+		elm.previousElementSibling.value = "";
+	});
+});
+
+$(".header .set-theme").on("click", function() {
+	var classBody = $("body").attr("class");
+	if (classBody === "dark-theme") {
+		$("body")
+			.removeClass("dark-theme")
+			.addClass("light-theme");
+		// Save data to sessionStorage
+		sessionStorage.setItem("myTheme", "light-theme");
+	} else {
+		$("body")
+			.removeClass("light-theme")
+			.addClass("dark-theme");
+		// Save data to sessionStorage
+		sessionStorage.setItem("myTheme", "dark-theme");
+	}
+});
+
+function myKeyPress(e) {
+	var keynum;
+
+	if (window.event) {
+		// IE
+		keynum = e.keyCode;
+	} else if (e.which) {
+		// Netscape/Firefox/Opera
+		keynum = e.which;
+	}
+
+	console.log(keynum);
+}
+$(document).keydown(function(event) {
+	myKeyPress(event);
+});
+
+var date = new Date();
+var year = date.getFullYear();
+$("#year-footer").text(year);
